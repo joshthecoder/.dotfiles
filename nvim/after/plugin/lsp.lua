8a50bb352e2ffb94a4901bbb5eeb5218c51fe54e
Josh Roesslein <jroesslein@nikola.tech>
2022-07-13 16:06:24 -0500

    lsp keymappings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,5 +1,9 @@
 local cmp = require("cmp")
 
+local Remap = require("joshthecoder.keymap")
+local nnoremap = Remap.nnoremap
+local inoremap = Remap.inoremap
+
 cmp.setup({
 	snippet = {
 		expand = function(args)
@@ -21,3 +25,43 @@ cmp.setup({
 		{ name = "buffer" },
 	},
 })
+
+local function config(_config)
+	return vim.tbl_deep_extend("force", {
+		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+		on_attach = function()
+			nnoremap("gd", function()
+				vim.lsp.buf.definition()
+			end)
+			nnoremap("K", function()
+				vim.lsp.buf.hover()
+			end)
+			nnoremap("<leader>vws", function()
+				vim.lsp.buf.workspace_symbol()
+			end)
+			nnoremap("<leader>vd", function()
+				vim.diagnostic.open_float()
+			end)
+			nnoremap("[d", function()
+				vim.diagnostic.goto_next()
+			end)
+			nnoremap("]d", function()
+				vim.diagnostic.goto_prev()
+			end)
+			nnoremap("<leader>vca", function()
+				vim.lsp.buf.code_action()
+			end)
+			nnoremap("<leader>vrr", function()
+				vim.lsp.buf.references()
+			end)
+			nnoremap("<leader>vrn", function()
+				vim.lsp.buf.rename()
+			end)
+			inoremap("<C-h>", function()
+				vim.lsp.buf.signature_help()
+			end)
+		end,
+	}, _config or {})
+end
+
+require("lspconfig").tsserver.setup(config())
